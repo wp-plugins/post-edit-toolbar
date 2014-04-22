4f49a60075cd8eed871bb7b01a4ef4e791e241c0
greghl <greghl@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2014-04-22 16:13:00 +0000

    Added plugin file + readme
    git-svn-id: https://plugins.svn.wordpress.org/post-edit-toolbar/trunk@900279 b8457f37-d9ea-0310-8a92-e5e31aec5664

---
+++ post-edit-toolbar.php
@@ -0,0 +1,123 @@
+<?php
+
+/*
+Plugin Name: Post Edit Toolbar
+Plugin URI: http://www.webyourbusiness.com
+Description: Adds most recently edited posts to the WordPress Toolbar for easy access
+Version: 1.2
+Author: Web Your Business
+Author URI: http://www.webyourbusiness.com/post-edit-toolbar
+
+Release Notes:
+
+1.2 - added drafters + separators
+1.1.1 - updated image included in the assets folder to be post-edit-tool-bar installed, not the page-edit-toolbar used as initial source
+1.1 - bug fix + add new post - added 'Add New Post' to top of list and fixed incorrect variables passed to get_posts() - they differ from get_pages()
+1.0 - initial release - based on Page Edit Toolbar by Jeremy Green
+*/
+
+add_action( 'admin_bar_menu', 'post_admin_bar_function', 999 );
+
+function post_admin_bar_function( $wp_admin_bar ) {
+
+        // parent post
+        $args = array(
+                'id' => 'post_list',
+                'title' => 'Post List',
+                'href' => home_url() . '/wp-admin/edit.php'
+        );
+        $wp_admin_bar->add_node( $args );
+
+        // top item in list is add new post
+        $args = array(
+                'id' => 'post_item_a',
+                'title' => 'Add New Post',
+                'parent' => 'post_list',
+                'href' => home_url() . '/wp-admin/post-new.php'
+        );
+        $wp_admin_bar->add_node( $args );
+
+	// separator from new to drafts
+        $args = array(
+                'id' => 'post_item_b',
+                'title' => '------------------------',
+                'parent' => 'post_list',
+                'href' => ''
+        );
+        $wp_admin_bar->add_node( $args );
+
+
+        // get list of drafts
+	$drafts_found = 'N';
+        $drafts = recently_edited_drafts();
+
+        foreach( $drafts as $draft ) {
+		$drafts_found = 'Y';
+
+		if ($draft->post_title == '') {
+			$draft->post_title = '[BLANK TITLE]';
+		}
+
+                // add child nodes (drafts recently edited)
+                $args = array(
+                        'id' => 'post_item_' . $draft->ID,
+                        'title' => '<strong><u>Draft</u>:</strong> '.$draft->post_title,
+                        'parent' => 'post_list',
+                        'href' => home_url() . '/wp-admin/post.php?post=' . $draft->ID . '&action=edit'
+                );
+                $wp_admin_bar->add_node( $args );
+
+        }
+
+        if ($drafts_found == 'Y') {
+		// separator from drafts to published
+        	$args = array(
+                	'id' => 'post_item_c',
+                	'title' => '------------------------',
+                	'parent' => 'post_list',
+                	'href' => ''
+        	);
+	        $wp_admin_bar->add_node( $args );
+	}
+
+
+        // get list of posts
+        $posts = recently_edited_posts();
+
+        // loop through up to 5 most recently modified posts
+        foreach( $posts as $post ) {
+
+                // add child nodes (posts to edit)
+                $args = array(
+                        'id' => 'post_item_' . $post->ID,
+                        'title' => $post->post_title,
+                        'parent' => 'post_list',
+                        'href' => home_url() . '/wp-admin/post.php?post=' . $post->ID . '&action=edit'
+                );
+                $wp_admin_bar->add_node( $args );
+
+        }
+
+}
+
+function recently_edited_drafts() {
+        $args = array(
+                'posts_per_page' => 2,
+                'sort_column' => 'post_modified',
+                'orderby' => 'post_date',
+                'post_status' => 'draft',
+                'order' => 'DESC'
+        );
+        $drafts = get_posts( $args );
+        return $drafts;
+}
+function recently_edited_posts() {
+        $args = array(
+                'posts_per_page' => 5,
+                'sort_column' => 'post_modified',
+                'orderby' => 'post_date',
+                'order' => 'DESC'
+        );
+        $posts = get_posts( $args );
+        return $posts;
+}
---
+++ readme.txt
@@ -0,0 +1,51 @@
+=== Post Edit Toolbar ===
+Contributors: WebYourBusiness
+Donate link: http://webyourbusiness.com/post-edit-toolbar
+Tags: edit post, toolbar, admin bar, list posts, list drafts 
+Requires at least: 3.0.1
+Tested up to: 3.9
+Stable tag: 1.2
+License: GPLv2 or later
+License URI: http://www.gnu.org/licenses/gpl-2.0.html
+
+Adds a dropdown to the WordPress toolbar of the 10 most recently edited pages. 
+
+== Description ==
+
+Adds a dropdown 'Post List' to the WordPress toolbar of the most recently edited drafts + posts. This gives you quick access to a work in progress, or a post that was recently published.
+
+This plugin is designed for publishers who make extensive use of posts and of particular use to those of us who get interrupted while composing posts!
+
+Currently it only displays an add-new post list, your 5 most recent drafts and 5 most recent published posts
+
+== Installation ===
+
+1. Upload `post-edit-toolbar.php` to the `/wp-content/plugins/` directory
+1. Activate the plugin through the 'Plugins' menu in WordPress
+1. A dropdown will appear in the WordPress toolbar titled 'Post List'.
+
+== Screenshots ==
+
+1. The dropdown shows the add new post, 2 most recently edited drafter, and the 5 most recently published posts.
+
+== Frequently Asked Questions ==
+
+For FAQs - see the post-edit-toolbar page at: http://webyourbusiness.com/post-edit-toolbar
+
+== Upgrade Notice ==
+
+= 1.2 =
+Added an 'Add drafts' - if you get interrupted while composing a post, you'll love access to drafts that I added in 1.2
+
+== Changelog ==
+
+= 0.1 =
+* Initial plugin submission
+= 1.0 =
+* initial release - based on Page Edit Toolbar by Jeremy Green
+= 1.1 =
+* bug fix + add new post - added 'Add New Post' to top of list and fixed incorrect variables passed to get_posts() - they differ from get_pages()
+= 1.1.1 =
+* updated image included in the assets folder to be post-edit-tool-bar installed, not the page-edit-toolbar used as initial source
+= 1.2 = 
+* added drafts + separators
